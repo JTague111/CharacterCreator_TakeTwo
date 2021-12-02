2e86bb363c7f4731357dc6a55f7416eb0609f412
Jonathan Tague <Jtague110@gmail.com>
2021-12-02 13:34:58 -0500

    fourth commit

--- CharacterCreator.Data/Character.cs
+++ CharacterCreator.Data/Character.cs
@@ -6,24 +6,49 @@ using System.Threading.Tasks;
 
 namespace CharacterCreator.Data
 {
-    public class Class1
+    public enum AdventurerClass
     {
-        enum Adventurerclass
-        {
-            Barbarian = 0,
-            Bard = 1,
-            Cleric,
-            Druid,
-            Fighter, 
-            Monk,
-            Paladin,
-            Ranger,
-            Rogue,
-            Sorcerer,
-            Warlock,
-            Wizard
-        }
+        Barbarian = 0,
+        Bard = 1,
+        Cleric,
+        Druid,
+        Fighter,
+        Monk,
+        Paladin,
+        Ranger,
+        Rogue,
+        Sorcerer,
+        Warlock,
+        Wizard
+    }
+    public enum Race
+    {
+        Dwarf = 0,
+        Elf = 1,
+        Halfling,
+        Human,
+        Dragonborn,
+        Gnome,
+        HalfElf,
+        HalfOrc,
+        Tiefling
+    }
+    public class Character
+    {
 
-        public enum adventurerclass { get; set; }
+        public Race ARace { get; set; }
+        public AdventurerClass AClass { get; set; }
+        public string Name { get; set; }
+        public string Background { get; set; }
+        public int StrScore { get; set; }
+        public int DexScore { get; set; }
+        public int ConScore { get; set; }
+        public int IntScore { get; set; }
+        public int WisScore { get; set; }
+        public int ChaScore { get; set; }
+        public string SavingThrow1 { get; set; }
+        public string SavingThrow2 { get; set; }
+        public int Level { get; }
+
     }
 }
