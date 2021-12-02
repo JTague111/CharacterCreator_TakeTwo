4846bd19531a57c1b4bd5dae105a838a0d060254
Jonathan Tague <Jtague110@gmail.com>
2021-12-02 13:31:04 -0500

    Third commit

--- CharacterCreator.Data/Character.cs
+++ CharacterCreator.Data/Character.cs
@@ -8,5 +8,22 @@ namespace CharacterCreator.Data
 {
     public class Class1
     {
+        enum Adventurerclass
+        {
+            Barbarian = 0,
+            Bard = 1,
+            Cleric,
+            Druid,
+            Fighter, 
+            Monk,
+            Paladin,
+            Ranger,
+            Rogue,
+            Sorcerer,
+            Warlock,
+            Wizard
+        }
+
+        public enum adventurerclass { get; set; }
     }
 }
