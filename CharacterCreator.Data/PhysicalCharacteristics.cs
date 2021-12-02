ac7eef76bbdf0123b391ed614e86aded5ec74fec
Jonathan Tague <Jtague110@gmail.com>
2021-12-02 14:14:59 -0500

    hopefully this works

--- CharacterCreator.Data/PhysicalCharacteristics.cs
+++ CharacterCreator.Data/PhysicalCharacteristics.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace CharacterCreator.Data
 {
-    class PhysicalCharacteristics
+    public enum HairColor
     {
+        Brown = 0,
+        Red = 1,
+        White,
+        Black,
+        Blonde
     }
+    public enum EyeColor
+    {
+        Hazel = 0,
+        Green,
+        Blue,
+        Silver,
+        Amber
+    }
+    public class PhysicalCharacteristics
+    {
+        public EyeColor Eyes { get; set; }
+        public HairColor Hair { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string SkinColor { get; set; }
+        public int Age { get; set; }
+    }
+
 }
